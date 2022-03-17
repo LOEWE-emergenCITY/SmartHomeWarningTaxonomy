e66b41fff602eb6771239ff2a7f96adf44cee157
Marc Wendelborn <marcwendelborn@web.de>
2022-03-17 23:08:06 +0100

    Implement schedule

--- SimulationLoader.py
+++ SimulationLoader.py
@@ -1,5 +1,14 @@
-class SimulationLoader:
-    def loadFile():
-        print('test')
+import json
+from datetime import date
+from apscheduler.schedulers.background import BlockingScheduler
 
-    loadFile() 
+def loadFile(text):
+    #with open('TestSimulation.json') as test_simulation_json:
+    #    data = json.load(test_simulation_json)
+    #    print(data["events"])
+    print(text)
+
+time = "2022-03-17 19:34:50"
+scheduler = BlockingScheduler(timezone="Europe/Berlin")
+scheduler.add_job(loadFile, 'date', run_date=time, args=['text'])
+scheduler.start()
