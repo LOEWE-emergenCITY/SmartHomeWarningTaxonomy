73e5f60c5e347ef3ca44791be441562b36ebd87b
Marc Wendelborn <marcwendelborn@web.de>
2022-05-04 15:57:08 +0200

    add .wav file

--- executors/acoustic_warning_executor.py
+++ executors/acoustic_warning_executor.py
@@ -1,2 +1,11 @@
+import pygame
+
 def execute_acoustic_warning(text):
-    print(text)
+
+    flag = True
+    while flag:
+        pygame.mixer.init()
+        pygame.mixer.music.load("mixkit-classic-alarm-995.wav")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy() == True:
+            continue
