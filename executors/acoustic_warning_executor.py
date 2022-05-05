f41aaf0cf64c5337235fde5ceb061f9b5c08a002
Marc Wendelborn <marcwendelborn@web.de>
2022-05-05 15:29:51 +0200

    Combine acoustic warning with display message

--- executors/acoustic_warning_executor.py
+++ executors/acoustic_warning_executor.py
@@ -1,11 +1,32 @@
 import pygame
+import tkinter
+from tkinter.messagebox import askyesno
+from threading import Thread
 
-def execute_acoustic_warning(text):
 
-    flag = True
-    while flag:
+def play_sound(id, stop):
+    while True:
         pygame.mixer.init()
         pygame.mixer.music.load("mixkit-classic-alarm-995.wav")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy() == True:
             continue
+        if stop():
+            break
+
+def execute_acoustic_warning(text):
+    stop_sound = False
+    for id in range(0,1):
+        sound = Thread(target=play_sound, args=(id, lambda: stop_sound))
+        sound.start()
+
+    parent = tkinter.Tk()  # Create the object
+    # Avoid it appearing and then disappearing quickly
+    parent.overrideredirect(1)
+    parent.withdraw()  # Hide the window as we do not want to see this one
+    answer = askyesno("test", "test 123", parent=parent)
+
+    if answer:
+        stop_sound = True
+
+execute_acoustic_warning('test')
