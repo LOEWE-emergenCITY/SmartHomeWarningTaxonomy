f9086d1c47438809b6c35af758d3db89d62184e1
Marc Wendelborn <marcwendelborn@web.de>
2022-05-10 22:08:22 +0200

    Connect gui with alert trigger

--- executors/acoustic_warning_executor.py
+++ executors/acoustic_warning_executor.py
@@ -1,8 +1,8 @@
 import pygame
-import tkinter
-from tkinter.messagebox import askyesno
+from gui.alert_dialog import Alert_Dialog
 from threading import Thread
 
+# To make it run on pi via ssh: export DISPLAY=":0"
 
 def play_sound(id, stop):
     while True:
@@ -15,18 +15,21 @@ def play_sound(id, stop):
             break
 
 def execute_acoustic_warning(text):
+    print('start')
     stop_sound = False
     for id in range(0,1):
         sound = Thread(target=play_sound, args=(id, lambda: stop_sound))
         sound.start()
 
-    parent = tkinter.Tk()  # Create the object
-    # Avoid it appearing and then disappearing quickly
-    parent.overrideredirect(1)
-    parent.withdraw()  # Hide the window as we do not want to see this one
-    answer = askyesno("test", "test 123", parent=parent)
+    dialog = Alert_Dialog("Das ist ein Test")
+    dialog.create_dialog()
 
-    if answer:
+    #parent = tkinter.Tk()  # Create the object
+    #parent.overrideredirect(1)
+    #parent.withdraw()  # Hide the window as we do not want to see this one
+    #answer = askyesno("test", "test 123", parent=parent)
+
+    if dialog.stop_alert:
         stop_sound = True
 
 execute_acoustic_warning('test')
---
+++ executors/gui/alert_dialog.py
@@ -0,0 +1,55 @@
+import tkinter as tk
+from tkinter import *
+
+# To make it run on pi via ssh: export DISPLAY=":0"
+
+class Alert_Dialog:
+    def __init__(self, event):
+        self.event = event
+        self.stop_alert = False
+
+    def store_rating(self, root):
+        self.stop_alert = True
+        root.destroy()
+    
+    def create_dialog(self):
+        root = tk.Tk()
+        root.title('Smart Home Systems Study')
+
+        # Make root window full screen
+        w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+        #root.attributes('-fullscreen', True)
+        root.geometry("%dx%d+0+0" % (w, h))
+        root.bind("<Escape>", lambda e: root.quit())
+
+        center_frame = tk.Frame(width=400, height=300)
+        center_frame.pack(expand=TRUE, ipady=100)
+
+        label = Label(center_frame, text=self.event, font=("Calibri", 44))
+        label.pack()
+
+        rating_frame = tk.Frame(center_frame, width=400, height=100)
+        rating_frame.pack(ipady=50)
+
+        # Rating Scale
+        label1 = Label(rating_frame, text="Bitte bewerte, ob du den Warnkanal für das Ereignis als angemessen empfindest.", font=("Calibri", 14))
+        label1.pack(side=TOP)
+
+        label2 = Label(rating_frame, text="1 = völlig ungeeignet, 5 = absolut passend", font=("Calibri", 14))
+        label2.pack(side=TOP)
+
+        button1 = Button(rating_frame, text='1', command=lambda : self.store_rating(root), width=4, height=3, background="#A53C3C", font=("Calibri", 25))
+        button1.pack(side=LEFT, padx=20)
+        button2 = Button(rating_frame, text='2', command=lambda : self.store_rating(root), width=4, height=3, background="#A53C3C", font=("Calibri", 25))
+        button2.pack(side=LEFT, padx=20)
+        button3 = Button(rating_frame, text='3', command=lambda : self.store_rating(root), width=4, height=3, background="#A53C3C", font=("Calibri", 25))
+        button3.pack(side=LEFT, padx=20)
+        button4 = Button(rating_frame, text='4', command=lambda : self.store_rating(root), width=4, height=3, background="#A53C3C", font=("Calibri", 25))
+        button4.pack(side=LEFT, padx=20)
+        button5 = Button(rating_frame, text='5', command=lambda : self.store_rating(root), width=4, height=3, background="#A53C3C", font=("Calibri", 25))
+        button5.pack(side=LEFT, padx=20)
+
+        root.mainloop()
+        
+#dialog = Alert_Dialog("Das ist ein Test")
+#dialog.create_dialog()
