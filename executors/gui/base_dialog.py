d7769aaf4e3ceae239ec42632f23db2155442df0
Marc Wendelborn <marcwendelborn@web.de>
2022-05-31 15:36:39 +0200

    Add study status to main screen

--- executors/gui/base_dialog.py
+++ executors/gui/base_dialog.py
@@ -4,12 +4,31 @@ from tkinter import *
 # To make it run on pi via ssh: export DISPLAY=":0"
 
 class Base_Dialog:
+
+    state = "Studie läuft"
+    
     def __init__(self, event):
-        self.event = event
+        self.study_runs = True
         self.stop_alert = False
 
-    def check_out(self, root):
-        root.destroy()
+    def change_study_status(self, label2, checkout_button):
+        if (self.study_runs):
+            self.study_runs = False
+            label2["text"] = "Studie unterbrochen"
+            checkout_button["text"] = "Check In"
+            label2.config(fg="red")
+        else:
+            self.study_runs = True
+            label2["text"] = "Studie läuft"
+            label2.config(fg="green")
+            checkout_button["text"] = "Check Out"
+        #self.study_status = "Studie unterbrochen"
+        #root.destroy()
+
+    def load_font_color(self):
+        if (self.study_status == "Studie läuft"):
+            return 'green'
+        return "red"
     
     def create_dialog(self):
         root = tk.Tk()
@@ -24,13 +43,21 @@ class Base_Dialog:
         center_frame = tk.Frame(width=400, height=300)
         center_frame.pack(expand=TRUE, ipady=50)
 
-        img = PhotoImage(file='executors\gui\peasec_logo.png')
-
+        img = PhotoImage(file='/home/pi/masterthesis/executors/gui/peasec_logo.png')
+        #img = PhotoImage(file='executors\gui\peasec_logo.png')
         label = Label(center_frame, image=img)
         label.pack(pady=20)
 
-        checkout_button = Button(center_frame, command=lambda : self.check_out(root), text="Check-Out", background="#000000", foreground="white", font=("Calibri", 25))   
-        checkout_button.pack(pady=50)
+        text_frame = tk.Frame(center_frame)
+        text_frame.pack(pady=20)
+
+        label1 = Label(text_frame, text="Status: ", font=("Calibri", 20))
+        label1.pack(pady=20, side=LEFT)
+        label2 = Label(text_frame, text="Studie läuft", fg="green", font=("Calibri", 20))
+        label2.pack(pady=20, side=LEFT)
+
+        checkout_button = Button(center_frame, command=lambda : self.change_study_status(label2, checkout_button), text="Check-Out", background="#000000", foreground="white", font=("Calibri", 25))   
+        checkout_button.pack(pady=20)
 
         root.mainloop()
 
