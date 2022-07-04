ff225edae6b7f1aefee02d432e2463422a17c771
Marc Wendelborn <marcwendelborn@web.de>
2022-07-04 13:25:53 +0200

    alert dialog draft

--- test.py
+++ test.py
@@ -1,22 +1,33 @@
-import datetime
-import time
-from util import load_simulation
+from textwrap import fill
+import tkinter as tk
+from tkinter import *
 
-def setup_scheduler():
-    simulation = load_simulation()
-    time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-    date = simulation['config']['date']
-    events = simulation['events']
+def abort_alert():
+    return
 
-    while len(events) != 0:
-        for event in events:
-            time_event = date + event['time']
-            if (time_event == time_now):
-                print("Event executed")
-                print(event['message'])
-        time.sleep(60)
-            
+root = tk.Tk()
+root.withdraw()
 
+window = tk.Toplevel()
+window.title('Smart Home Systems Study')
+w, h = window.winfo_screenwidth(), window.winfo_screenheight()
+#window.attributes('-fullscreen', True)
+window.geometry("%dx%d+0+0" % (w, h))
+window.bind("<Escape>", lambda e: window.quit())
 
-print(datetime.datetime.now())
-setup_scheduler()
+center_frame = tk.Frame(window, width=300, height=300)
+center_frame.pack(expand=TRUE, ipady=50)
+
+text_label = Label(center_frame, width=100, text="Die Sicherung der Kaffeemaschine ist \n durchgebrant Die Sicherung ist durchgebrannt!", font=("Calibri", 30))
+text_label.pack()
+
+button_frame = tk.Frame(center_frame, width=300)
+button_frame.pack(ipady=20)
+
+acknowledge_button = Button(button_frame, command=lambda : abort_alert(), text="Alarm wahrgenommen", background="green", foreground="white", font=("Calibri", 25))   
+acknowledge_button.pack(side=LEFT, padx=20)
+
+abort_button = Button(button_frame, command=lambda : abort_alert(), text="Abbruch", background="red", foreground="white", font=("Calibri", 25))   
+abort_button.pack(side=LEFT, padx=20)
+
+window.mainloop()
